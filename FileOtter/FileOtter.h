8227e5ee7b1bffbb344c36a582fdc93452a75976
Sami Samhuri <sami@samhuri.net>
2024-04-24 19:06:47 -0700

    Initial Commit

---
+++ FileOtter/FileOtter.h
@@ -0,0 +1,18 @@
+//
+//  FileOtter.h
+//  FileOtter
+//
+//  Created by Sami Samhuri on 2024-04-24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FileOtter.
+FOUNDATION_EXPORT double FileOtterVersionNumber;
+
+//! Project version string for FileOtter.
+FOUNDATION_EXPORT const unsigned char FileOtterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileOtter/PublicHeader.h>
+
+
